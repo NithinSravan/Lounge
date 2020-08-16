176ffe3fcb7cf9f4ec44eb20d0f4523d400696a4
NithinSravan <56395258+NithinSravan@users.noreply.github.com>
2020-08-16 19:58:22 +0530

    minor

--- models/user.js
+++ models/user.js
@@ -73,41 +73,7 @@ const userSchema = new mongoose.Schema({
     default:0
   }
 });
-// userSchema.methods.generateAuthToken = async function () {
-//   const user = this
-//   const token = jwt.sign({ email:user.email,_id: user._id.toString() }, 'mybestkeptsecret',{expiresIn:"1h"})
 
-//   user.tokens = user.tokens.concat({ token })
-//   await user.save()
-
-//   return token
-// }
-
-// userSchema.statics.findByCredentials = async (email, password) => {
-//   const user = await User.findOne({ email })
-
-//   if (!user) {
-//       throw new Error('Unable to login')
-//   }
-
-//   const isMatch = await bcrypt.compare(password, user.password)
-
-//   if (!isMatch) {
-//       throw new Error('Unable to login')
-//   }
-
-//   return user
-// }
-
-// userSchema.pre('save', async function (next) {
-//   const user = this
-
-//   if (user.isModified('password')) {
-//       user.password = await bcrypt.hash(user.password, 8)
-//   }
-
-//   next()
-// })
 userSchema.plugin(uniqueValidator);
 
 module.exports=mongoose.model("User",userSchema);
